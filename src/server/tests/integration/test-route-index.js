f061faf1d765fc79cf34ab5c89722b3c56dfc72c
Jamie Kelly <jamie@studio174.ca>
2016-07-28 16:04:35 -0700

    removes integration tests

--- src/server/tests/integration/test-route-index.js
+++
@@ -1,9 +0,0 @@
-var supertest = require('supertest'),
-  app = require('../../app');
-
-exports.homepage_should_200 = function(done) {
-  supertest(app)
-    .get('/')
-    .expect(200)
-    .end(done);
-};
