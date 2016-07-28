2d273d740a2adfc94717fb21c723092298272156
Jamie Kelly <jamie@studio174.ca>
2016-07-28 16:05:14 -0700

    adds user model

---
+++ src/server/models/user.js
@@ -0,0 +1,29 @@
+/**
+ * User model
+ */
+
+'use strict';
+
+// require mongoose for data modeling 
+var mongoose = require('mongoose'),
+
+  // schema class 
+  Schema = mongoose.Schema,
+
+  // passport's local strat 
+  passportLocalMongoose = require('passport-local-mongoose');
+
+
+// defines our user schema 
+var userSchema = new Schema({
+  username: String,
+  password: String
+});
+
+// plugs into our user schema to take care of salting and hashing the password 
+userSchema.plugin(passportLocalMongoose);
+
+// first param is the singular name for the collection, 
+// mongoose automatically looks for the plural version
+// so our db will have a 'users' collection
+module.exports = mongoose.model('User', userSchema);
