148926494519349631e5a7cd43aa582f59b71858
Jamie Kelly <jamie@studio174.ca>
2016-08-03 18:04:32 -0700

    adds jwt

--- src/server/app.js
+++ src/server/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('../../creds.json'); // get our config file
+
 //  using passports local strategy for authentication     
 var passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
@@ -14,6 +17,8 @@ var passport = require('passport'),
   User = require('./models/user.js');
 
 var routes = require('./routes/index');
+var auth = require('./routes/api/authenticate');
+var api = require('./routes/api/index');
 var user = require('./routes/api/user');
 
 var app = express();
@@ -21,12 +26,11 @@ var app = express();
 // require database connection every time the server boots 
 require('./lib/connection');
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+
+app.set('superSecret', config.secret); // secret variable
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));    
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -43,7 +47,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, '../client')));
-// console.log(path.join(__dirname, 'src/client'));
 
 // configure passport 
 passport.use(new LocalStrategy(User.authenticate()));
@@ -51,8 +54,51 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use('/', routes);
-app.use('/api/users', user);
+app.use('/api', auth);
 
+
+
+/*
+* Begin protected API
+*/
+
+app.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+
+  }
+
+});
+
+app.use('/api', api);
+app.use('/api/users', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -66,7 +112,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500).json({
       message: err.message,
       error: err
@@ -74,14 +120,16 @@ if (app.get('env') === 'development') {
   });
 }
 
+
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500).json({
     message: err.message,
     error: {}
   });
 });
+
 
 
 module.exports = app;
